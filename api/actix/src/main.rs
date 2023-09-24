a27ffdbb43ffc6662c6a25bc8247f8046988520f
Kristofers Solo <kristians.cagulis@pm.me>
2023-09-24 11:41:17 +0300

    refactor: move migrations folder

--- api/actix/src/main.rs
+++ api/actix/src/main.rs
@@ -3,6 +3,7 @@ mod routes;
 use actix_web::{web::Data, App, HttpServer};
 use api_models::{pool, AppState};
 use routes::auth::{login, register};
+use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -10,6 +11,12 @@ async fn main() -> std::io::Result<()> {
     dotenv::dotenv().ok();
 
     let pool = pool().await.unwrap();
+
+    let mut base_path = env::current_dir().expect("Failed to get current directory");
+    base_path.push("migrations");
+
+    let migrator = sqlx::migrate::Migrator::new(base_path).await.unwrap();
+    migrator.run(&pool).await.unwrap();
 
     HttpServer::new(move || {
         App::new()
