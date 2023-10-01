33347311d1c2e5438996a1ca807b5791daf9dab3
Kristofers Solo <kristians.cagulis@pm.me>
2023-10-01 20:26:59 +0300

    fix(auth): change auth url
    Now all auth relatate urls are available at `/auth/[login/register/logout]`

--- api/actix/src/apps/auth/urls.rs
+++ api/actix/src/apps/auth/urls.rs
@@ -3,7 +3,7 @@ use actix_web::{web, Scope};
 use super::views::{login, logout, register};
 
 pub fn url() -> Scope {
-    web::scope("")
+    web::scope("/auth")
         .service(web::resource("/register").route(web::post().to(register)))
         .service(web::resource("/login").route(web::get().to(login)))
         .service(web::resource("/logout").route(web::get().to(logout)))
